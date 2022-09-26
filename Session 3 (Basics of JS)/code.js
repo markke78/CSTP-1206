ce15929280fc4067625b42d98f643f8363441915
Mark Ke <markke78@hotmail.com>
2022-09-26 10:48:06 -0700

    assignment done

---
+++ Session 3 (Basics of JS)/code.js
@@ -0,0 +1,112 @@
+// 1. Write a function to find the sum of the elements inside the array
+const sumOfArray = (nums) => console.log(nums.reduce((a, b)=> a + b));
+
+const arr1 = [1, 2, 3, 4, 5];
+sumOfArray(arr1);
+// 2. Using a function for loop print all even numbers up to n (parameter in function);
+
+const evenNum = (n) =>{
+    for (let i = 0; i < n + 1; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+
+evenNum(20);
+
+// 3. Write a function take a string as a parameter, Check if a string contains the letter “y”. If yes print "YES" and only one time.
+    // For example - "Crayzy" "Yes"
+const checkY = (letters) => {
+
+    for (let i of letters.toLowerCase()) {
+        if ( i === "y"){
+            console.log("Yes");
+            break;
+        }
+    }
+}
+
+checkY("CreaYzY");
+
+// 4. Write a function to find the factorial of a given number n
+
+const factorial = (n) => (n <= 1) ? 1 : n * factorial(n - 1);
+     
+console.log(factorial(5));
+
+
+// 5. // Write a function which accepts 4 parameters (different scores of a student for subjects), and
+// then calculate the average of all the score.
+// If the average is more than 90 , then console grade A
+// If the average is between 70 and 90 , then console grade B
+// If the average is between 50-70, then console grade C
+// Other wise console grade F
+
+
+
+const gradeCheck = (subA, subB, subC, subD) => {
+    const gradeArr = []; 
+    gradeArr.push(subA, subB, subC, subD);
+    console.log(gradeArr.reduce((a, b) => (a + b)) / gradeArr.length) ;
+}
+
+const java = 90;
+const math = 88;
+const python = 92;
+const database = 95;
+
+gradeCheck(java, math, python, database);
+
+// 6. Write a function which prints the star pattern like that for the given value n
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+
+const starPinter1 = n => {
+    let starts = "";
+    for (let i = 0; i < n; i++){
+        console.log(starts += "*");
+    }
+}
+
+starPinter1(7);
+
+// 7.
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+const starPinter2 = n => {
+    let starts = "";
+    for (let i = 0; i < n; i++){
+        console.log(starts += "*");
+    }
+    for (let i = 0; i < n; i++){
+        console.log(starts.slice(i, -1));
+    }
+}
+
+starPinter2(7);
+
+// 8. Write a function to reverse a string
+const reverseStr = (aStr) => console.log(aStr.split("").reverse().join(""));
+
+
+reverseStr("werfg");
