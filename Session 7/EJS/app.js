96f06538d131980e6746221004a1ac76013cedf2
Mark Ke <markke78@hotmail.com>
2022-10-25 03:18:16 -0700

    finished some contain

--- Session 7/EJS/app.js
+++ Session 7/EJS/app.js
@@ -4,4 +4,16 @@ const PORT = 5000;
 
 app.set("view engine", "ejs");
 
-app.get("/")
+// let listOfName = ["Prabh", "Daniel", "MaC"]
+
+let html = "<h1> Hello!! I am HTML<h1>"
+
+app.get("/", (req, res)=>{
+  res.render("index", {name: html})
+})
+
+
+
+app.listen(PORT, ()=>{
+  console.log(`Server Running at port ${PORT}`);
+})
